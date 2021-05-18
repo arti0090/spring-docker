f6315bd2d06005202fcc89eb148bfed45e03965a
arti0090 <arti0090@gmail.com>
2021-05-18 13:17:15 +0200

    init

---
+++ src/main/java/com/helloworld/hellowolrd/HelloWorldApplication.java
@@ -0,0 +1,29 @@
+package com.helloworld.hellowolrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name)
+	{
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/helloworld/hellowolrd/HelloWolrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloworld.hellowolrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWolrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
